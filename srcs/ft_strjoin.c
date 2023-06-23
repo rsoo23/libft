313c345d478871d19cf8d370628f0765815c5744
Soo Rong Jie <81731553+rsoo23@users.noreply.github.com>
2023-06-23 09:03:56 +0800

    Update ft_strjoin.c

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -15,51 +15,51 @@
 // Note: any function is used, compile all the files together at once
 
 // ver2:
-char	*ft_strjoin(const char *s1, const char *s2)
-{
-	char			*res;
-	unsigned int	i;
-	size_t			len1;
-	size_t			len2;
-
-	i = 0;
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	res = malloc(len1 + len2 + 1);
-	if (!res)
-		return (NULL);
-	while (i++ < len1)
-		res[i - 1] = s1[i - 1];
-	i = 0;
-	while (i++ < len2)
-		res[i - 1 + len1] = s2[i - 1];
-	res[i - 1 + len1] = '\0';
-	free((char *)s1);
-	return (res);
-}
-
-// ver1:
-// char	*ft_strjoin(char const *s1, char const *s2)
+// char	*ft_strjoin(const char *s1, const char *s2)
 // {
 // 	char			*res;
 // 	unsigned int	i;
-// 	unsigned int	j;
+// 	size_t			len1;
+// 	size_t			len2;
 
 // 	i = 0;
-// 	j = 0;
-// 	if (!s1 || !s2)
-// 		return (0);
-// 	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+// 	len1 = ft_strlen(s1);
+// 	len2 = ft_strlen(s2);
+// 	res = malloc(len1 + len2 + 1);
 // 	if (!res)
-// 		return (0);
-// 	while (s1[i])
-// 		res[j++] = s1[i++];
+// 		return (NULL);
+// 	while (i++ < len1)
+// 		res[i - 1] = s1[i - 1];
 // 	i = 0;
-// 	while (s2[i])
-// 		res[j++] = s2[i++];
-// 	res[j] = '\0';
+// 	while (i++ < len2)
+// 		res[i - 1 + len1] = s2[i - 1];
+// 	res[i - 1 + len1] = '\0';
+// 	free((char *)s1);
 // 	return (res);
 // }
+
+// ver1:
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*res;
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!res)
+		return (0);
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	return (res);
+}
 
 // int main()
 // {
